dd6dc2d1944c8b4dd535efa4ea7951997c2c9e3d
jeremy millet <jeremy.millet37@gmail.com>
2023-06-24 17:53:50 +0200

    no overlay on about hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,7 @@ function Hero(props) {
     const img = props.img
     return (
         <div className="hero">
-            <div className={img === '/static/media/background-img-2.abab3f12f4cb82f1dfa1.png' ? 'hero-overlay hero-overlay-about':'hero-overlay'}>
+            <div className={img === '/static/media/background-img-2.abab3f12f4cb82f1dfa1.png' ? 'hero-overlay-about':'hero-overlay'}>
             </div>
             <img className={img === '/static/media/background-img-2.abab3f12f4cb82f1dfa1.png' ? 'hero-img hero-about':'hero-img'} src={props.img} alt="" />
             <h1  className='hero-text'>{props.text}</h1>
